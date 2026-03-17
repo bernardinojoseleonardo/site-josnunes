91d84483ea7ca4fb085bfbba8882f91c1cddb2a3
Bernardinojoseleonardo <beejoseleonardo@gmail.com>
2026-03-17 20:36:50 +0100

    Robust database configuration for Render

--- josnunes/settings.py
+++ josnunes/settings.py
@@ -70,11 +70,12 @@ WSGI_APPLICATION = 'josnunes.wsgi.application'
 # https://github.com/jakubroztocil/django-database-url
 import dj_database_url
 
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
-        conn_max_age=600
-    )
+    'default': db_from_env if db_from_env else {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
